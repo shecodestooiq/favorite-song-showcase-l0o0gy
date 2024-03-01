d015c88d869344b7ce05ef81bc88dc51ad7f2a98
l0o0gy <minamahone9620@gmail.com>
2024-03-02 00:08:37 +0300

    add new task

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function displaySongInfo() {
+        
+    const songName = document.getElementById("songName").value;
+    const youtubeLink = document.getElementById("youtubeLink").value;
+    const description = document.getElementById("description").value;
+    const year = document.getElementById("year").value;
+    
+
+// the display-area
+songName = document.getElementById("songName").innerHTML=favsong;
+description = document.getElementById("description").innerHTML=songDescription;
+year = document.getElementById("year").innerHTML=year;    
+innerHTML<iframe src="https://www.youtube.com//embed/"+(youtubeLink = document.getElementById("youtubeLink").innerHTML=link)></iframe>
 
 }
 module.exports = {
     displaySongInfo: displaySongInfo,
-};
+ };
